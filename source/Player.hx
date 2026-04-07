2efb13da3a668ea83919861ee7517e9106a96b8e
Marcosayfi <plushigaming555@gmail.com>
2026-04-07 10:11:08 +0300

    fixed formatting of Player.hx (NEEDED)

--- source/Player.hx
+++ source/Player.hx
@@ -4,75 +4,84 @@ import flixel.FlxSprite;
 import flixel.FlxG;
 
 class Player extends FlxSprite
- {
-   static inline var SPEED_X:Float = 100;
-   static inline var FLAP_Y:Float = -200;
-   static inline var GRAVITY:Float = 400;
-   public var skin:String;
+{
+  static inline var SPEED_X:Float = 100;
+  static inline var FLAP_Y:Float = -200;
+  static inline var GRAVITY:Float = 400;
+  public var skin:String;
 
-    public function new(x:Float = 50, y:Float = 200)
+  public function new(x:Float = 50, y:Float = 200)
+  {
+    super(x, y);
+
+    // Load saved skin from FlxSave
+    var save = new flixel.util.FlxSave();
+    if (save.bind("haxelbird") && save.data.playerSkin != null)
     {
-      super(x, y);
-
-      // Load saved skin from FlxSave
-      var save = new flixel.util.FlxSave();
-      if (save.bind("haxelbird") && save.data.playerSkin != null)
-        {
-          skin = save.data.playerSkin;
-          save.flush();
-        } else
-        {
-          skin = "normal"; // default
-          save.flush();
-        }
-
-      if (skin == "normal")
-        loadGraphic("assets/images/gameplay/skins/duckie.png"); // skins
-      else if (skin == "other") // TODO: fix the amount of if statements
-       loadGraphic("assets/images/gameplay/skins/other duck.png");
-      else if (skin == "extremeQuality")
-       loadGraphic("assets/images/gameplay/skins/HIGH QUALITY.png");
-      else if (skin == "creeper")
-       loadGraphic("assets/images/gameplay/skins/creeper.png");
-      else if (skin == "devilangel")
-       loadGraphic("assets/images/gameplay/skins/devilangel.png");
-      else if (skin == "zombie")
-       loadGraphic("assets/images/gameplay/skins/zombie.png");
-    acceleration.y = GRAVITY;
-    maxVelocity.y = 300;
+     skin = save.data.playerSkin;
+     save.flush();
+    }
+    else
+    {
+      skin = "normal"; // default
+      save.flush();
     }
 
+    if (skin == "normal")      
+    {
+      loadGraphic("assets/images/gameplay/skins/duckie.png");
+    }
+    else if (skin == "other")
+    {
+      loadGraphic("assets/images/gameplay/skins/other duck.png");
+    }
+    else if (skin == "extremeQuality")
+    {  
+      loadGraphic("assets/images/gameplay/skins/HIGH QUALITY.png");
+    }
+    else if (skin == "creeper")
+    {
+      loadGraphic("assets/images/gameplay/skins/creeper.png");
+    }
+    else if (skin == "devilangel")
+    {
+      loadGraphic("assets/images/gameplay/skins/devilangel.png");
+    }
+    else if (skin == "zombie")
+    {
+      loadGraphic("assets/images/gameplay/skins/zombie.png");
+    }
+
+    acceleration.y = GRAVITY;
+    maxVelocity.y = 300;
+  }
+
     override function update(elapsed:Float)
-    { 
+    {
+     #if !mobile
+     // flap
+     if (FlxG.keys.justPressed.SPACE || FlxG.keys.justPressed.UP || FlxG.keys.justPressed.W)
+      {
+        jumpFunction();
+      }
+     #end
 
-      #if !mobile
-        // flap
-        if (FlxG.keys.justPressed.SPACE || FlxG.keys.justPressed.UP || FlxG.keys.justPressed.W)
-        {
-          jumpFunction();
-        }
-      #end
+     super.update(elapsed);
+      // stop at top
+      if (y < -87)
+      {
+        FlxG.switchState(DeathState.new);
+      }
 
-
-
-        super.update(elapsed);
-
-        // stop at top
-        if (y < -87)
-        {
-            FlxG.switchState(DeathState.new);
-        }
-
-        // stop at bottom
-        if (y + height > FlxG.height)
+      // stop at bottom
+      if (y + height > FlxG.height)
         {
             y = FlxG.height - height;
         }
-     }
+    }
 
-  public function jumpFunction()
-  {
-    velocity.y = FLAP_Y;
-  }
-
+    public function jumpFunction()
+    {
+      velocity.y = FLAP_Y;
+    }
 }
